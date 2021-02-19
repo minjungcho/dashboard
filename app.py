b25046cf59217f7506555c19a00ffd41211c43aa
minjungcho <34115655+minjungcho@users.noreply.github.com>
2021-02-19 10:30:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,49 @@
 
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('My first app')
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+
+# Line Chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# Plot a map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+#  Selectbox
+# option = st.selectbox(
+#     'Which number do you like best?',
+#      df['first column'])
+#
+# 'You selected: ', option
+
+
+#  Widget
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected:', option
