f79d631005c1e6291d502e78825f73eb528b11de
minjungcho <34115655+minjungcho@users.noreply.github.com>
2021-02-22 17:10:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,47 +3,60 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title('의약품유통업체리스트')
+import memsql
+import MySQLdb 
+import sys
+import os
+import pymysql
+import base64
+import requests
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+#AWS RDS Info
+#host = 'app-sfa.cilpkhhykq9i.ap-northeast-2.rds.amazonaws.com'
+#port = '3306'
+#username = "sfaadmin"
+#database = "sfa"
+#password = "P@ssw0rd1!"
 
+#def connect_RDS(host,port,username,password,database):
+#    try:
+#        conn = pymysql.connect(host,port=port,user=ursername,passwd=password,db=database,use_unicode=True,
+#                              charset ='utf8')
+#        cursor = conn.cursor()
+#    except:
+#        logging.error("RDS에 연결되지 않았습니다")
+#        sys.exit(1)
+#    return conn,cursor
 
-# Line Chart
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+#singlestore sfa db
+import pymysql
+connection = pymysql.connect(host='13.124.198.70', user='dbadmin', password='quintet', port = 3306, db = 'sfa')
+cursor = connection.cursor()
+#try:
+#    with connection.cursor() as cursor:  
+#        sql = "select * from T_OPTY"
+#        cursor.execute("set names utf8")
+#        cursor.execute(sql)
+#        result = cursor.fetchall()
+        
+#        for i in result:
+#            print(i)
+#finally:
+#    connection.close()
 
-st.line_chart(chart_data)
+#dict형식의 cursor
+cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-# Plot a map
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+#sql작성
+mysql = "select * from T_SERIES"
+#mysql = "select * from t_reference"
 
-st.map(map_data)
+#sql실행
+cursor.execute(query=mysql)
 
+#결과가져오기
+result = cursor.fetchall() #또는 result = cursor.fetchone()
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+df_T_SERIES = pd.DataFrame(result)
 
-    st.line_chart(chart_data)
-
-#  Selectbox
-# option = st.selectbox(
-#     'Which number do you like best?',
-#      df['first column'])
-#
-# 'You selected: ', option
-
-
-#  Widget
-option = st.sidebar.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected:', option
+df_T_SERIES['ATTRIBUTE_11']=df_T_SERIES.replace('ATTRIBUTE_11','END_DT' - 'START_DT')
