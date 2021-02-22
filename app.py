f6c2d3620ae09f3a3f59e08ad51ddb2fa22f6343
minjungcho <34115655+minjungcho@users.noreply.github.com>
2021-02-22 17:11:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -52,3 +52,68 @@ mysql = "select * from T_SERIES"
 
 #sql실행
 cursor.execute(query=mysql)
+
+
+#결과가져오기
+result = cursor.fetchall() #또는 result = cursor.fetchone()
+
+df_T_SERIES = pd.DataFrame(result)
+
+df_T_SERIES['ATTRIBUTE_11']=df_T_SERIES.replace('ATTRIBUTE_11','END_DT' - 'START_DT')
+
+
+
+
+
+
+
+
+
+
+
+
+st.title('의약품유통업체리스트')
+
+#df = pd.DataFrame({
+#  'first column': [1, 2, 3, 4],
+#  'second column': [10, 20, 30, 40]
+#})
+
+
+# Line Chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# Plot a map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+#  Selectbox
+# option = st.selectbox(
+#     'Which number do you like best?',
+#      df['first column'])
+#
+# 'You selected: ', option
+
+
+#  Widget
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected:', option
+
